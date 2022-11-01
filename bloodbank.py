28aa6d11f8277f003c23f2710be5b1a23e671181
DILEESH-PV <dileeshpv1998@gmail.com>
2022-11-01 19:18:01 +0530

    first commit

---
+++ bloodbank.py
@@ -0,0 +1,21 @@
+while True:
+    print("\nSelect an option")
+    print("1 add a donor")
+    print("2 view all donors")
+    print("3 search a donor")
+    print("4 update the donor")
+    print("5 delete a donor")
+    print("6 exit")
+    ch=int(input("select an option  : \n"))
+    if (ch==1):    
+        print("selected add donor")
+    elif(ch==2):
+         print("selected view all donors")
+    elif(ch==3):
+        print("selected search donor")
+    elif(ch==4):
+        print("selected update the donor")
+    elif(ch==5):
+        print("selected delete donor")
+    elif(ch==6):
+        break
