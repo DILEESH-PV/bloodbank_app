5fb5bdc49907fe3467dcb6b90aa77e4340ec520b
DILEESH-PV <dileeshpv1998@gmail.com>
2022-11-01 20:24:27 +0530

    connection created

--- bloodbank.py
+++ bloodbank.py
@@ -1,20 +1,35 @@
+import mysql.connector
+
+mydb=mysql.connector.connect(host='localhost',user='root',password='',database='bloodbankdb')
+
+mycursor=mydb.cursor()
 while True:
     print("\nSelect an option")
     print("1 add a donor")
     print("2 view all donors")
     print("3 search a donor")
-    print("4 update the donor")
-    print("5 delete a donor")
+    print("4 update  donor")
+    print("5 delete  donor")
     print("6 exit")
     ch=int(input("select an option  : \n"))
     if (ch==1):    
-        print("selected add donor")
+        name=input("Enter the name")
+        address=input("Enter address")
+        phno=input("Enter the phone number")
+        age=input("Enter the age")
+        sex=input("Enter the sex")
+        bg=input("Enter the blood group")
+        sql='INSERT INTO `donor`(`name`, `address`, `phno`, `age`, `sex`, `bloodgp`) VALUES (%s,%s,%s,%s,%s,%s)'
+        data=(name,address,phno,age,sex,bg)
+        mycursor.execute(sql,data)
+        mydb.commit()        
+        print("inserted success")
     elif(ch==2):
          print("selected view all donors")
     elif(ch==3):
-        print("selected search donor")
+        print("selected search a donor")
     elif(ch==4):
-        print("selected update the donor")
+        print("selected update  donor")
     elif(ch==5):
         print("selected delete donor")
     elif(ch==6):
